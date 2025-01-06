a44e0ecc980f5aea588c9e500f7d125d7456bd75
purplenib <breadjoo1014@naver.com>
2025-01-06 19:57:51 +0900

    FESI1-15 [Refactor]: 버튼 type 속성 추가 & 중복 class 제거

--- src/components/@shared/Button.tsx
+++ src/components/@shared/Button.tsx
@@ -1,38 +1,35 @@
 import clsx from 'clsx';
 
 type ButtonProps = {
-  variant?: 'primary' | 'secondary';
+  type?: 'button' | 'submit' | 'reset';
   size?: 'large' | 'small' | 'full';
   font?: '16' | '14';
+  variant?: 'primary' | 'secondary';
   children: React.ReactNode;
 } & React.ComponentPropsWithoutRef<'button'>;
 
 export default function Button({
   size,
-  variant = 'primary',
+  type = 'button',
   font = '16',
+  variant = 'primary',
   children,
   className,
   disabled,
   ...props
 }: ButtonProps) {
   const buttonClass = clsx(
-    'base-class',
+    'rounded-lg p-[10px] text-center',
     {
-      'm-auto rounded-lg p-[10px] text-center':
-        variant === 'primary' || variant === 'secondary',
       'bg-orange-600 text-white hover:bg-orange-500 focus:bg-orange-700':
         !disabled && variant === 'primary',
       'border-[1px] border-orange-600 bg-white text-orange-600 hover:border-orange-500 hover:text-orange-500 focus:border-orange-700 focus:text-orange-700':
         !disabled && variant === 'secondary',
-
       'bg-gray-400 text-white': disabled && variant === 'primary',
       'border-[1px] border-gray-400 text-gray-400':
         disabled && variant === 'secondary',
-
       'text-base font-semibold': font === '16',
       'text-sm font-semibold': font === '14',
-
       'h-11 w-[332px]': size === 'large',
       'h-10 w-[120px]': size === 'small',
       'w-full': size === 'full',
@@ -41,12 +38,7 @@ export default function Button({
   );
 
   return (
-    <button
-      type="button"
-      className={buttonClass}
-      disabled={disabled}
-      {...props}
-    >
+    <button type={type} className={buttonClass} disabled={disabled} {...props}>
       {children}
     </button>
   );
