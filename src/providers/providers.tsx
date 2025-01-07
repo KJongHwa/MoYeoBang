1c9c9a655f4398c9e0eb26e56daa18e2340da60c
purplenib <breadjoo1014@naver.com>
2025-01-07 11:01:23 +0900

    [Chore]: recoil 제거

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,17 +1,14 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RecoilRoot } from "recoil";
 import { useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         {children}
       </QueryClientProvider>
-    </RecoilRoot>
   );
 }
