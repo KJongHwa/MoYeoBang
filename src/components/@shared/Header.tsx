bb8b0327cb394f7f92263c33ccfb9d6ccb269bd6
지솔레일 <janggh1012@gmail.com>
2025-01-13 17:34:09 +0900

    FESI1-14 [Design]: dropdown 마이페이지, 로그아웃 버튼 구현

--- src/components/@shared/Header.tsx
+++ src/components/@shared/Header.tsx
@@ -4,6 +4,7 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 import Button from './Button';
 
@@ -16,6 +17,7 @@ export default function Header() {
   const handleSearching = () => {
     setSearching(!searching);
   };
+
   const navLinks = [
     { label: '모임 찾기', href: '/' },
     { label: '찜한 모임', href: '/likes' },
@@ -25,18 +27,23 @@ export default function Header() {
     ? { src: '/search.png', alt: '검색버튼' }
     : { src: '/search_delete.png', alt: '검색닫기버튼' };
 
+  const liDropdowns = [{ label: '마이페이지' }, { label: '로그아웃' }];
+
   return (
     <div className="relative z-10">
       <div className="fixed top-0 w-full">
         <div className="bg-secondary-bg mx-auto flex h-[52px] w-full max-w-[1920px] items-center justify-between border-b px-5 md:h-[60px] md:px-[30px] xl:h-[60px] xl:px-[200px]">
           {/* Navigation Links */}
           <nav className="text-text-default flex items-center gap-8 text-base">
-            <Image
-              src="/Logo_Large.png"
-              width={100}
-              height={23}
-              alt="로고 이미지"
-            />
+            <Link href="/">
+              <Image
+                src="/Logo_Large.png"
+                width={100}
+                height={23}
+                alt="로고 이미지"
+              />
+            </Link>
+
             {navLinks.map((link) => (
               <Link
                 key={link.href}
@@ -78,15 +85,40 @@ export default function Header() {
                 </Link>
               </div>
             ) : (
-              <div className="flex text-base font-bold text-white">
+              <div className="group relative flex text-base font-bold text-white">
                 <Link href="/mypage">
                   <Image
                     src="/profile_image_default.png"
                     width={40}
                     height={40}
                     alt="마이페이지 이미지"
+                    className="cursor-pointer"
                   />
                 </Link>
+                <ul className="bg-secondary-80 absolute -right-11 top-full z-50 mt-2 hidden w-32 rounded-md shadow-md group-hover:pointer-events-auto group-hover:block md:-right-10">
+                  {liDropdowns.map((liDropdown) => (
+                    <li key={liDropdown.label}>
+                      {liDropdown.label === '마이페이지' ? (
+                        <Link href="/mypage">
+                          <button
+                            type="button"
+                            className="hover:bg-secondary-60 w-full px-4 py-2 text-left"
+                          >
+                            {liDropdown.label}
+                          </button>
+                        </Link>
+                      ) : (
+                        <button
+                          onClick={handleLogin}
+                          type="button"
+                          className="hover:bg-secondary-60 w-full px-4 py-2 text-left"
+                        >
+                          {liDropdown.label}
+                        </button>
+                      )}
+                    </li>
+                  ))}
+                </ul>
               </div>
             )}
             <Image
