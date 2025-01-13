5ad4b849459232ea9b3b26908922e9e5c4be75dc
purplenib <breadjoo1014@naver.com>
2025-01-13 12:23:51 +0900

    FESI1-39 [Feat]: 테마의 장르, 지역, 날짜, 난이도 필터 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,34 @@
+'use client';
+
+import { useState } from 'react';
+import { mockGatherings } from '@/data/mockGatherings';
+
 import CreateGatheringBtn from '@/components/gathering/CreateGatheringBtn';
 import GatheringCard from '@/components/gathering/GatheringCard';
 import GatheringFilters from '@/components/gathering/GatheringFilters';
-import GenreFilters from '@/components/gathering/GenreFilters';
-
-import { mockGatherings } from '@/data/mockGatherings';
 
 export default function Gathering() {
+  const [filteredGatherings, setFilteredGatherings] = useState(mockGatherings);
+
   return (
     <div className="mx-auto flex h-full max-w-6xl flex-col gap-12 px-4 py-32 md:py-48">
       <header className="mx-1 flex flex-col gap-2 text-white xl:mx-5">
         <p className="text-sm font-medium">함께 할 사람이 없나요?</p>
         <h1 className="text-2xl font-semibold">지금 여기로 모여방</h1>
       </header>
+
       <main className="flex flex-col gap-10 md:gap-12">
-        <section className="mx-1 flex flex-col xl:mx-5">
-          <div className="flex flex-col gap-6">
-            <GenreFilters />
-            <div className="flex justify-between text-text-secondary">
-              <GatheringFilters />
-              <ul>정렬</ul>
-            </div>
-          </div>
-        </section>
+        <GatheringFilters
+          gatherings={mockGatherings}
+          setFilteredGatherings={setFilteredGatherings}
+        />
 
         <section className="mx-auto grid h-full w-full grid-cols-1 gap-3 text-white xl:grid-cols-2">
-          {mockGatherings.map((gathering: any) => (
+          {filteredGatherings.map((gathering: any) => (
             <GatheringCard key={gathering.gatheringId} {...gathering} />
           ))}
         </section>
+
         <CreateGatheringBtn />
       </main>
     </div>
---
+++ src/components/gathering/GenreFilter.tsx
@@ -0,0 +1,33 @@
+import { genres } from '@/constants/themeList';
+
+interface GenreFilterProps {
+  onGenreChange: (value: string) => void;
+  selectedGenre: string;
+}
+
+export default function GenreFilter({
+  onGenreChange,
+  selectedGenre,
+}: GenreFilterProps) {
+  const handleGenreClick = (value: string) => {
+    onGenreChange(value);
+  };
+
+  const buttonClass = (isSelected: boolean) =>
+    isSelected ? 'text-white customUnderline' : 'text-text-disabled';
+
+  return (
+    <ul className="scrollbar-hidden flex  gap-3 overflow-x-auto overflow-y-hidden whitespace-nowrap text-lg font-semibold">
+      {genres.map((genre) => (
+        <button
+          key={genre.value}
+          type="button"
+          onClick={() => handleGenreClick(genre.value)}
+          className={buttonClass(selectedGenre === genre.value)}
+        >
+          {genre.label}
+        </button>
+      ))}
+    </ul>
+  );
+}
