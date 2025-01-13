855afcf3b9b3c2a1c40ff88de1f4ac6d19d56b83
purplenib <breadjoo1014@naver.com>
2025-01-13 14:31:52 +0900

    FESI1-39 [Refactor]: 모임 찾기 페이지 use client 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,19 @@
-'use client';
-
-import { useState } from 'react';
 import { mockGatherings } from '@/data/mockGatherings';
 
 import CreateGatheringBtn from '@/components/gathering/CreateGatheringBtn';
-import GatheringCard from '@/components/gathering/GatheringCard';
-import GatheringFilters from '@/components/gathering/GatheringFilters';
+import GatheringList from '@/components/gathering/GatheringList';
 
 export default function Gathering() {
-  const [filteredGatherings, setFilteredGatherings] = useState(mockGatherings);
+  const gatherings = mockGatherings;
 
   return (
     <div className="mx-auto flex h-full max-w-6xl flex-col gap-12 px-4 py-32 md:py-48">
       <header className="mx-1 flex flex-col gap-2 text-white xl:mx-5">
-        <p className="text-sm font-medium">함께 할 사람이 없나요?</p>
-        <h1 className="text-2xl font-semibold">지금 여기로 모여방</h1>
+        <h1 className="order-2 text-2xl font-semibold">지금 여기로 모여방</h1>
+        <p className="order-1 text-sm font-medium">함께 할 사람이 없나요?</p>
       </header>
-
       <main className="flex flex-col gap-10 md:gap-12">
-        <GatheringFilters
-          gatherings={mockGatherings}
-          setFilteredGatherings={setFilteredGatherings}
-        />
-
-        <section className="mx-auto grid h-full w-full grid-cols-1 gap-3 text-white xl:grid-cols-2">
-          {filteredGatherings.map((gathering: any) => (
-            <GatheringCard key={gathering.gatheringId} {...gathering} />
-          ))}
-        </section>
-
+        <GatheringList gatherings={gatherings} />
         <CreateGatheringBtn />
       </main>
     </div>
