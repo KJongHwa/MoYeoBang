a527316a689f3e8c89cc6b65c6e6e37fc4398e6b
purplenib <breadjoo1014@naver.com>
2025-01-14 14:11:12 +0900

    FESI1-16 [Design]: 공통 Input, TextArea 브랜드 컬러 적용

--- src/components/gathering/GatheringCard.tsx
+++ src/components/gathering/GatheringCard.tsx
@@ -27,7 +27,7 @@ export default function GatheringCard({
   return (
     <figure className="relative">
       <Link href={`/gathering/${gatheringId}`}>
-        <div className="bg-brand-tertiary flex max-h-32 w-full rounded-xl md:max-h-[170px]">
+        <div className="flex max-h-32 w-full rounded-xl bg-default-tertiary md:max-h-[170px]">
           <AlarmBadge hour={extractHour(registrationEnd)} />
           <Image
             src={image}
@@ -35,7 +35,7 @@ export default function GatheringCard({
             width={240}
             height={170}
             quality={100}
-            className="bg-brand-secondary w-28 rounded-l-xl md:w-60"
+            className="w-28 rounded-l-xl bg-default-tertiary md:w-60"
           />
           <div className="mx-3 my-2 flex flex-1 flex-col justify-between md:mx-6 md:my-4">
             <div className="flex justify-between">
