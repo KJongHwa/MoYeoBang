9d52e287655b35505da078d2f37d993158af5adc
KJongHwa <dblls0724@naver.com>
2025-01-14 16:28:55 +0900

    Refector : AllReview 페이지 리팩토링 진행

--- src/app/allreview/page.tsx
+++ src/app/allreview/page.tsx
@@ -1,44 +1,15 @@
-'use client';
-
 import HeaderTitle from '@/components/@shared/HeaderTitle';
 import PageContainer from '@/components/@shared/PageContainer';
-import RatingSection from '@/components/allReview/RatingSection';
-import ReviewSection from '@/components/allReview/ReviewSection';
-import GenreFilter from '@/components/@shared/GenreFilter';
-import { useState } from 'react';
+import AllReviewList from '@/components/allReview/AllReviewList';
+import { mockReviews } from '@/data/mockReviews';
 
 export default function AllReview() {
-  const [selectedGenre, setSelectedGenre] = useState<string>('all');
-  const [selectedLocation, setSelectedLocation] = useState<string>('all');
-  const [selectedDate, setSelectedDate] = useState<string>('');
-  const [selectedSort, setSelectedSort] = useState<string>('createdAt');
+  const allReviews = mockReviews;
 
   return (
     <PageContainer>
       <HeaderTitle title="모든 리뷰" content="모여방 리뷰를 확인해 보세요" />
-
-      <section className="relative py-12">
-        <GenreFilter
-          onGenreChange={setSelectedGenre}
-          selectedGenre={selectedGenre}
-        />
-      </section>
-
-      <section className="relative border-b-[1px] border-solid border-[#646464] pb-4 md:pb-6">
-        <RatingSection selectedGenre={selectedGenre} />
-      </section>
-
-      <section className="relative top-12 pb-10">
-        <ReviewSection
-          selectedGenre={selectedGenre}
-          selectedLocation={selectedLocation}
-          selectedDate={selectedDate}
-          selectedSort={selectedSort}
-          onLocatingChange={setSelectedLocation}
-          onDateChange={setSelectedDate}
-          onSortingChange={setSelectedSort}
-        />
-      </section>
+      <AllReviewList allReviews={allReviews} />
     </PageContainer>
   );
 }
--- src/components/likes/LikesGatheringList.tsx
+++ src/components/likes/LikesGatheringList.tsx
@@ -17,6 +17,41 @@ export default function LikesGatheringList({
     return genreMatches;
   });
 
+  const renderReviews = () => {
+    if (filteredGatherings.length === 0) {
+      return (
+        <section key={selectedGenre} className="mt-10 flex flex-col gap-6">
+          <EmptyElement>
+            <p>아직 찜한 모임이 없어요</p>
+            <p>지금 바로 마음에 드는 모임을 찜해보세요</p>
+          </EmptyElement>
+        </section>
+      );
+    }
+    return (
+      <section
+        key={selectedGenre}
+        className="mx-auto grid h-full w-full grid-cols-1 gap-3 pb-10 text-white xl:grid-cols-2"
+      >
+        {filteredGatherings.map((gathering) => (
+          <GatheringCard
+            key={gathering.gatheringId}
+            gatheringId={gathering.gatheringId}
+            name={gathering.name}
+            location={gathering.location}
+            themeName={gathering.themeName}
+            image={gathering.image}
+            level={gathering.level}
+            dateTime={gathering.dateTime}
+            registrationEnd={gathering.registrationEnd}
+            capacity={gathering.capacity.toString()}
+            participantCount={gathering.participantCount.toString()}
+          />
+        ))}
+      </section>
+    );
+  };
+
   return (
     <>
       <section className="relative py-12">
@@ -26,35 +61,7 @@ export default function LikesGatheringList({
         />
       </section>
 
-      {filteredGatherings.length === 0 ? (
-        <section key={selectedGenre} className="mt-10 flex flex-col gap-6">
-          <EmptyElement>
-            <p>아직 찜한 모임이 없어요</p>
-            <p>지금 바로 마음에 드는 모임을 찜해보세요</p>
-          </EmptyElement>
-        </section>
-      ) : (
-        <section
-          key={selectedGenre}
-          className="mx-auto grid h-full w-full grid-cols-1 gap-3 pb-10 text-white xl:grid-cols-2"
-        >
-          {filteredGatherings.map((gathering) => (
-            <GatheringCard
-              key={gathering.gatheringId}
-              gatheringId={gathering.gatheringId}
-              name={gathering.name}
-              location={gathering.location}
-              themeName={gathering.themeName}
-              image={gathering.image}
-              level={gathering.level}
-              dateTime={gathering.dateTime}
-              registrationEnd={gathering.registrationEnd}
-              capacity={gathering.capacity.toString()}
-              participantCount={gathering.participantCount.toString()}
-            />
-          ))}
-        </section>
-      )}
+      {renderReviews()}
     </>
   );
 }
