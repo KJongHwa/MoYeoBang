7ee0093d1af0ae988061b86d0a3f85fcd234c64f
지솔레일 <janggh1012@gmail.com>
2025-01-15 10:23:03 +0900

    FESI-19 [Refactor]: <EmptyElement>사용 컴포넌트 코드 정리

--- src/components/mypage/myCreateGathering.tsx
+++ src/components/mypage/myCreateGathering.tsx
@@ -4,31 +4,29 @@ import MyCreateGatheringDetail from './myGatheringCard/myCreateGatheringDetail';
 import EmptyElement from '../@shared/EmptyElement';
 
 interface MyCreateGatheringProps {
-  userID: number; //
+  userID: number;
 }
 export default function MyCreateGathering({ userID }: MyCreateGatheringProps) {
   const userGatherings = mockGatherings.filter(
     (gathering: any) => gathering.userId === userID
   );
-
+  if (userGatherings.length === 0) {
+    return <EmptyElement>아직 만든 모임이 없어요</EmptyElement>;
+  }
   return (
     <div className="flex flex-col gap-5">
-      {userGatherings.length === 0 ? (
-        <EmptyElement>아직 만든 모임이 없어요</EmptyElement>
-      ) : (
-        userGatherings.map((gathering: any) => (
-          <MyGatheringCard key={gathering.gatheringId} image={gathering.image}>
-            <MyCreateGatheringDetail
-              location={gathering.location}
-              dateTime={gathering.dateTime}
-              name={gathering.name}
-              themeName={gathering.themeName}
-              capacity={gathering.capacity}
-              participantCount={gathering.participantCount}
-            />
-          </MyGatheringCard>
-        ))
-      )}
+      {userGatherings.map((gathering: any) => (
+        <MyGatheringCard key={gathering.gatheringId} image={gathering.image}>
+          <MyCreateGatheringDetail
+            location={gathering.location}
+            dateTime={gathering.dateTime}
+            name={gathering.name}
+            themeName={gathering.themeName}
+            capacity={gathering.capacity}
+            participantCount={gathering.participantCount}
+          />
+        </MyGatheringCard>
+      ))}
     </div>
   );
 }
--- src/components/mypage/myReviewWrite.tsx
+++ src/components/mypage/myReviewWrite.tsx
@@ -3,23 +3,22 @@ import MyReviewCard from './myReviewCard';
 import EmptyElement from '../@shared/EmptyElement';
 
 export default function MyReviewWrite() {
+  if (mockReviews.length === 0) {
+    <EmptyElement>아직 작성한 리뷰가 없어요</EmptyElement>;
+  }
   return (
     <div className="mt-10 flex flex-col gap-6">
-      {mockReviews.length === 0 ? (
-        <EmptyElement>아직 작성한 리뷰가 없어요</EmptyElement>
-      ) : (
-        mockReviews.map((review) => (
-          <div key={review.reviewId}>
-            <MyReviewCard
-              score={review.score}
-              comment={review.comment}
-              createdAt={review.createdAt}
-              Gathering={review.Gathering}
-              User={review.User}
-            />
-          </div>
-        ))
-      )}
+      {mockReviews.map((review) => (
+        <div key={review.reviewId}>
+          <MyReviewCard
+            score={review.score}
+            comment={review.comment}
+            createdAt={review.createdAt}
+            Gathering={review.Gathering}
+            User={review.User}
+          />
+        </div>
+      ))}
     </div>
   );
 }
