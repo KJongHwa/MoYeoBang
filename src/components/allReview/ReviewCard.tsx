c90f6c97a3412a3df1941cc77a595a28caf0130d
KJongHwa <dblls0724@naver.com>
2025-01-17 14:24:40 +0900

    Fix : 배포 3차 QA 관련 수정 요청 반영

--- src/components/allReview/ReviewCard.tsx
+++ src/components/allReview/ReviewCard.tsx
@@ -2,6 +2,7 @@ import { ReviewDto } from '@/types/review.types';
 import { yearMonthDay } from '@/utils/dateUtils';
 import Image from 'next/image';
 import Rating from '../@shared/Rating';
+import ReviewEditDropdown from './ReviewEditDropdown';
 
 export default function ReviewCard({
   score,
@@ -22,7 +23,7 @@ export default function ReviewCard({
           className="rounded-t-[20px] md:rounded-l-[20px] md:rounded-r-[0px]"
         />
       </div>
-      <div className="flex w-full flex-col gap-[13px] px-[25px] py-5">
+      <div className="flex w-full flex-col justify-between gap-[13px] px-[25px] py-5">
         <div className="flex flex-col gap-2">
           <Rating rating={score} width={120} height={24} />
           <h2 className="pb-3 text-xs font-medium text-secondary-50">
@@ -30,7 +31,7 @@ export default function ReviewCard({
           </h2>
           <p className="text-sm font-medium text-white">{comment}</p>
         </div>
-        <div className="flex items-center md:justify-normal xl:justify-end">
+        <div className="flex items-center justify-end">
           <Image
             src={User.image}
             alt={User.nickname}
@@ -46,6 +47,7 @@ export default function ReviewCard({
             {yearMonthDay(createdAt)}
           </p>
         </div>
+        <ReviewEditDropdown score={score} comment={comment} />
       </div>
     </article>
   );
