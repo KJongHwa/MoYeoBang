9f743548b89425857a4a0f0854437fcf50745dc5
purplenib <breadjoo1014@naver.com>
2025-01-17 15:36:02 +0900

    FESI1-20 [Design]: 퍼즐 아이콘 버튼 영역 확장

--- src/components/gathering/PuzzleButton.tsx
+++ src/components/gathering/PuzzleButton.tsx
@@ -62,22 +62,28 @@ export default function PuzzleButton({
     });
   };
   return (
-    <div className="absolute right-2 top-1 md:right-6 md:top-4">
-      <button type="button" onClick={handleFavoriteToggle}>
-        <Image
-          src={
-            isFavorited ? '/icons/puzzle_full.svg' : '/icons/puzzle_line.svg'
-          }
-          alt="퍼즐 아이콘"
-          width={24}
-          height={24}
-          quality={100}
-          className={clsx('object-contain', {
-            'h-4 w-4 p-1 md:h-6 md:w-6 md:p-0': layout === 'card',
-            'h-6 w-6 p-4 md:p-0': layout === 'slot',
-          })}
-        />
-      </button>
-    </div>
+    <button
+      type="button"
+      onClick={handleFavoriteToggle}
+      className={clsx(
+        'absolute right-0  flex h-10 w-10 flex-1 items-center justify-center',
+        {
+          '-top-1 md:right-3 md:top-2': layout === 'card',
+          'top-0 m-3 md:right-4 md:top-3 md:m-0': layout === 'slot',
+        }
+      )}
+    >
+      <Image
+        src={isFavorited ? '/icons/puzzle_full.svg' : '/icons/puzzle_line.svg'}
+        alt="퍼즐 아이콘"
+        width={24}
+        height={24}
+        quality={100}
+        className={clsx('object-contain', {
+          'h-4 w-4 md:h-6 md:w-6': layout === 'card',
+          'h-6 w-6': layout === 'slot',
+        })}
+      />
+    </button>
   );
 }
