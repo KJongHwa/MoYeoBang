d26da8da96a1e40c7c91690ad3bd22cae3724bd3
purplenib <breadjoo1014@naver.com>
2025-01-22 23:42:39 +0900

    FESI1-40 [Refactor]: api 호출 쿼리 문자열 개선

--- src/utils/apiCall.ts
+++ src/utils/apiCall.ts
@@ -2,6 +2,10 @@ import { axiosInstance } from '@/axios/axiosInstance';
 
 type HttpMethod = 'get' | 'post' | 'patch' | 'delete' | 'put';
 
+type Params = {
+  [key: string]: string | number | boolean | null | undefined;
+};
+
 // 공통 apiCall 함수
 export const apiCall = async (
   method: HttpMethod,
@@ -10,7 +14,18 @@ export const apiCall = async (
   config: Record<string, any> = {}
 ) => {
   const { params, ...axiosConfig } = config;
-  const queryString = new URLSearchParams(params).toString();
+
+  // params가 있을 경우 빈 값을 제외하고 쿼리 문자열 생성
+  const queryString = params
+    ? new URLSearchParams(
+        Object.entries(params)
+          .filter(
+            ([, value]) => value !== '' && value !== undefined && value !== null
+          )
+          .map(([key, value]) => [key, String(value)])
+      ).toString()
+    : '';
+
   const finalUrl = queryString ? `${url}?${queryString}` : url;
 
   const response = await axiosInstance[method](finalUrl, data, axiosConfig);
