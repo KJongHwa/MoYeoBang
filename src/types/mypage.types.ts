25a5245abeb52dfab4cf574b78ff8093d39af395
지솔레일 <janggh1012@gmail.com>
2025-01-23 09:43:49 +0900

    [Refactor] : params type 파일 리팩토링

--- src/types/mypage.types.ts
+++ src/types/mypage.types.ts
@@ -22,3 +22,16 @@ export interface UserTypes {
 export interface ImageTypes {
   url: string;
 }
+
+export interface GetUserInfoParmas {
+  userId: string | number;
+}
+
+export interface AddImageFileParams {
+  image: File;
+}
+
+export interface UpdateMyProfileParams {
+  nickname: string;
+  image: string;
+}
