7da617bca9ed98c16989fd9184ff6f1dc7505923
지솔레일 <janggh1012@gmail.com>
2025-01-24 09:44:41 +0900

    FESI1-63 [Feat] : 나의 모임 api 연동

--- src/components/mypage/myGathering.tsx
+++ src/components/mypage/myGathering.tsx
@@ -1,15 +1,38 @@
 import { mockGatherings } from '@/data/mockGatherings';
+import { useQuery } from '@tanstack/react-query';
+import { UserGatheringJoined } from '@/types/mypage.types';
+import { getMyGatheringJoied } from '@/axios/mypage/api';
 import MyGatheringCard from './myGatheringCard';
 import MyGatheringDetail from './myGatheringCard/myGatheringDetail';
 import EmptyElement from '../@shared/EmptyElement';
 
 export default function MyGathering() {
-  if (mockGatherings.length === 0) {
+  const { data: gatherings, isLoading: isGatheringLoading } = useQuery<
+    UserGatheringJoined[]
+  >({
+    queryKey: ['myGatheringJoined'],
+    queryFn: getMyGatheringJoied,
+  });
+
+  if (isGatheringLoading) {
+    return (
+      <div className="flex h-dvh items-center justify-center">Loading...</div>
+    );
+  }
+
+  if (!gatherings) {
+    return (
+      <div className="flex h-dvh items-center justify-center">
+        모임 정보를 불러올 수 없습니다.
+      </div>
+    );
+  }
+  if (gatherings.length === 0) {
     return <EmptyElement>신청한 모임이 아직 없어요</EmptyElement>;
   }
   return (
     <div className="flex flex-col gap-5">
-      {mockGatherings.map((gathering: any) => (
+      {gatherings.map((gathering: any) => (
         <MyGatheringCard
           key={gathering.gatheringId}
           image={gathering.image}
