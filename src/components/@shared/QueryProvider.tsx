00f56e691a099ac65f2fbaae44c300613d5e1959
purplenib <breadjoo1014@naver.com>
2025-01-27 17:12:40 +0900

    FESI1-40 [Chore]: 리액트 쿼리 Devtools 설치 및 적용

--- src/components/@shared/QueryProvider.tsx
+++ src/components/@shared/QueryProvider.tsx
@@ -1,12 +1,31 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            throwOnError: true,
+            refetchOnWindowFocus: false,
+            retryOnMount: true,
+            refetchOnReconnect: false,
+            retry: 3,
+          },
+        },
+      })
+  );
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools
+        initialIsOpen={process.env.NEXT_PUBLIC_MODE === 'local'}
+      />
+    </QueryClientProvider>
   );
 }
