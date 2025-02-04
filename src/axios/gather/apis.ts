fec1cce65db540ac4301d858e88e85b4956ef891
purplenib <breadjoo1014@naver.com>
2025-02-04 09:30:17 +0900

    FESI1-87 [Refactor]: authAxiosInstance 및 apiCall 함수 적용

--- src/axios/gather/apis.ts
+++ src/axios/gather/apis.ts
@@ -1,9 +1,8 @@
-import { publicAxiosInstance } from '@/axios/axiosInstance';
 import type {
   GatheringRequestBody,
   GatheringUrlParams,
 } from '@/types/gathering.types';
-import qs from 'qs';
+import { apiCall } from '@/utils/apiCall';
 import { API_PATH } from '../config/path';
 
 // 홈페이지 모임 목록 (registrationEnd 기준)
@@ -13,16 +12,7 @@ export const getGatheringsByRegistrationEnd = async () => {
     sortBy: 'registrationEnd',
   };
 
-  const queryString = qs.stringify(params, {
-    skipNulls: true,
-    arrayFormat: 'brackets',
-    filter: (prefix, value) => (value === '' ? undefined : value),
-  });
-
-  const response = await publicAxiosInstance.get(
-    `${API_PATH.gathering.default}?${queryString}`
-  );
-  return response.data;
+  return apiCall('get', API_PATH.gathering.default, null, { params });
 };
 
 // 홈페이지 모임 목록 (participantCount 기준)
@@ -32,37 +22,15 @@ export const getGatheringsByParticipantCount = async () => {
     sortBy: 'participantCount',
   };
 
-  const queryString = qs.stringify(params, {
-    skipNulls: true,
-    arrayFormat: 'brackets',
-    filter: (prefix, value) => (value === '' ? undefined : value),
-  });
-
-  const response = await publicAxiosInstance.get(
-    `${API_PATH.gathering.default}?${queryString}`
-  );
-  return response.data;
+  return apiCall('get', API_PATH.gathering.default, null, { params });
 };
 
 // 모임 목록
 export const getGatherings = async (params: GatheringUrlParams) => {
-  const queryString = qs.stringify(params, {
-    skipNulls: true,
-    arrayFormat: 'brackets',
-    filter: (prefix, value) => (value === '' ? undefined : value),
-  });
-
-  const response = await publicAxiosInstance.get(
-    `${API_PATH.gathering.default}?${queryString}`
-  );
-  return response.data;
+  return apiCall('get', API_PATH.gathering.default, null, { params });
 };
 
 // 모임 생성
 export const postGathering = async (data: GatheringRequestBody['post']) => {
-  const response = await publicAxiosInstance.post(
-    API_PATH.gathering.default,
-    data
-  );
-  return response.data;
+  return apiCall('post', API_PATH.gathering.default, data);
 };
