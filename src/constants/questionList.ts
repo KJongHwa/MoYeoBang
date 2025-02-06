222e117e863a0ea32f5d196f507a5cece836daf2
purplenib <breadjoo1014@naver.com>
2025-02-06 17:22:06 +0900

    FESI1-90 [Design]: 테마 추천 페이지 디자인 시안 적용

--- src/constants/questionList.ts
+++ src/constants/questionList.ts
@@ -1,24 +1,28 @@
 export const questions = [
   {
     text: '선호하는 지역을 선택해주세요.',
-    options: ['건대', '홍대', '혜화', '강남', '상관없어요'],
+    options: ['건대', '홍대', '혜화', '강남', '🙂‍↔️ 상관없어요'],
   },
   {
-    text: '평소 관심있던 장르는 무엇인가요?',
+    text: '오늘 저녁, 이 중 하나의 음료를 마셔야한다면?',
     options: [
-      '스릴러 또는 자극적인 것',
-      '오락적인 것',
-      '감성적인 것',
-      '이색적인 것',
+      '🍷 포도 향이 진한 와인',
+      '🍺 시원한 맥주',
+      '🍵 따뜻한 차',
+      '🍸 영롱한 빛의 칵테일',
     ],
   },
   {
-    text: '원하는 플레이 타임을 선택해주세요.',
-    options: ['길게', '짧게', '상관없어요'],
+    text: '내가 원하는 플레이 방식은?',
+    options: [
+      '🐶 한 번 잡은 문제는 놔주지 않는 불독 스타일',
+      '🦅 답답한 건 못참는 시원시원한 독수리 스타일',
+      '🐸 게임 그 자체를 즐기는 개구리 스타일',
+    ],
   },
   {
     text: '원하는 난이도를 선택해주세요.',
-    options: ['쉬움', '보통', '어려움', '상관없어요'],
+    options: ['쉬움', '보통', '어려움', '🙂‍↔️ 상관없어요'],
   },
 ];
 
@@ -28,25 +32,25 @@ export const locationMap: { [key: string]: string } = {
   홍대: 'hongdae',
   혜화: 'hyehwa',
   강남: 'gangnam',
-  상관없어요: '',
+  '🙂‍↔️ 상관없어요': '',
 };
 
 export const genreMap: { [key: string]: string[] } = {
-  '스릴러 또는 자극적인 것': ['mystery', 'horror', 'thriller', 'stealth'],
-  '오락적인 것': ['comic', 'drama'],
-  '감성적인 것': ['romance', 'drama', 'fantasy'],
-  '이색적인 것': ['fantasy', 'sci-fi'],
+  '🍷 포도 향이 진한 와인': ['mystery', 'horror', 'thriller', 'stealth'],
+  '🍺 시원한 맥주': ['comic', 'drama'],
+  '🍵 따뜻한 차': ['romance', 'drama', 'fantasy'],
+  '🍸 영롱한 빛의 칵테일': ['fantasy', 'sci-fi'],
 };
 
 export const levelMap: { [key: string]: string } = {
   쉬움: 'low',
   보통: 'middle',
   어려움: 'high',
-  상관없어요: '',
+  '🙂‍↔️ 상관없어요': '',
 };
 
 export const playTimeMap: { [key: string]: string[] } = {
-  짧게: ['60', '65'],
-  길게: ['70', '75'],
-  상관없어요: ['all'],
+  '🐶 한 번 잡은 문제는 놔주지 않는 불독 스타일': ['60', '65'],
+  '🦅 답답한 건 못참는 시원시원한 독수리 스타일': ['70', '75'],
+  '🐸 게임 그 자체를 즐기는 개구리 스타일': ['60', '65', '70', '75'],
 };
