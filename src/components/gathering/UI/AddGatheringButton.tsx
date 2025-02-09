1016443c33f9bbdbd95a9dfde9599ad78d995971
purplenib <breadjoo1014@naver.com>
2025-02-10 03:36:15 +0900

    FESI1-100 [Feat]: gatheringEdit 페이지로 이동

--- src/components/gathering/UI/AddGatheringButton.tsx
+++ src/components/gathering/UI/AddGatheringButton.tsx
@@ -1,33 +1,33 @@
 'use client';
 
 import Image from 'next/image';
-import { useModal } from '@/hooks/useModal';
+import { useRouter } from 'next/navigation';
 import { useAuthNavigation } from '@/hooks/useAuthNavigation';
 import ButtonMotion from '@/components/@shared/animation/ButtonMotion';
-import GatheringModal from '@/components/gathering/GatheringModal';
 
 export default function CreateGatheringBtn() {
-  const { isOpen, openModal, closeModal } = useModal();
-  const { checkAndNavigate } = useAuthNavigation('/login', openModal);
+  const router = useRouter();
+
+  const navigateToEditPage = () => {
+    router.push('/gathering/edit');
+  };
+
+  const { checkAndNavigate } = useAuthNavigation('/login', navigateToEditPage);
 
   return (
-    <>
-      <ButtonMotion
-        type="button"
-        className="fixed bottom-12 right-8 xl:bottom-16 xl:right-1/10"
-        onClick={checkAndNavigate}
-      >
-        <Image
-          src="/icons/creator.svg"
-          alt="모임 생성 아이콘"
-          width={64}
-          height={64}
-          quality={100}
-          className="h-14 w-14 md:h-16 md:w-16"
-        />
-      </ButtonMotion>
-
-      {isOpen && <GatheringModal isOpen={isOpen} onClose={closeModal} />}
-    </>
+    <ButtonMotion
+      type="button"
+      className="fixed bottom-12 right-8 xl:bottom-16 xl:right-1/10"
+      onClick={checkAndNavigate}
+    >
+      <Image
+        src="/icons/creator.svg"
+        alt="모임 생성 아이콘"
+        width={64}
+        height={64}
+        quality={100}
+        className="h-14 w-14 md:h-16 md:w-16"
+      />
+    </ButtonMotion>
   );
 }
