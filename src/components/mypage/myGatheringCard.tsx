e601b36ea5263257758b6b0deb0cdb80fe793f49
지솔레일 <janggh1012@gmail.com>
2025-02-10 17:44:11 +0900

    FESI1-70 [Fix] : myGatheringCard 반응형 레이아웃 수정

--- src/components/mypage/myGatheringCard.tsx
+++ src/components/mypage/myGatheringCard.tsx
@@ -25,13 +25,13 @@ export default function MyGatheringCard({
   return (
     <figure className="relative flex w-full flex-col gap-2 rounded-2xl bg-default-tertiary xs:max-h-[170px] xs:flex-row">
       {isCanceled && (
-        <p className="absolute left-24 top-32 z-10 text-lg font-bold text-white xs:left-20  xs:top-40 md:left-3 md:top-16 ">
+        <p className="absolute left-20 top-32 z-10 text-lg font-bold text-white xs:left-3  xs:top-16 md:left-3 md:top-16 ">
           모집 취소된 모임이에요.
         </p>
       )}
       <div
         className={clsx(
-          'relative rounded-t-2xl md:max-w-[192px] md:rounded-l-2xl md:rounded-r-none',
+          'relative rounded-t-2xl xs:rounded-l-2xl xs:rounded-r-none md:max-w-[192px]',
           {
             'bg-status-hover opacity-20 ': isCanceled,
           }
@@ -45,7 +45,7 @@ export default function MyGatheringCard({
             height={170}
             quality={100}
             onError={handleError}
-            className={`h-[292px] w-full rounded-t-2xl bg-default-tertiary xs:h-[170px] xs:w-[192px] md:rounded-l-2xl md:rounded-r-none ${isError ? 'object-cover' : ''}`}
+            className={`h-[292px] w-full rounded-t-2xl bg-default-tertiary xs:h-[170px] xs:w-[192px] xs:rounded-l-2xl xs:rounded-r-none ${isError ? 'object-cover' : ''}`}
           />
         </Link>
       </div>
