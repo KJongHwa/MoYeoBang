3d74277bf1998b4d46d99b21702d314c5f8f24fd
purplenib <breadjoo1014@naver.com>
2025-02-12 17:04:42 +0900

    FESI1-105 [Design]: Toast 디자인 재적용

--- src/components/@shared/Toast.tsx
+++ src/components/@shared/Toast.tsx
@@ -18,7 +18,13 @@ export default function Toast({ message, type }: ToastProps) {
   return (
     <aside
       role="alert"
-      className="fixed bottom-28 left-1/2 z-100 w-11/12 -translate-x-1/2 translate-y-full transform rounded-lg border-2 border-primary-5 bg-default-tertiary p-4 shadow-lg transition-transform duration-300 ease-in-out md:w-fit"
+      className={clsx(
+        'fixed bottom-28 left-1/2 z-100 w-11/12 -translate-x-1/2 translate-y-full transform rounded-lg border-2  bg-default-tertiary p-4 shadow-lg transition-transform duration-300 ease-in-out md:w-fit',
+        {
+          'border-primary-40': type === 'success',
+          'border-status-danger': type === 'error',
+        }
+      )}
     >
       <div className="flex justify-center gap-2">
         {type === 'success' ? <CheckBox /> : <Warning />}
